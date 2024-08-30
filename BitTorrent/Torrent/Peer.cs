f55bfdf62ad9f8544f4f63c835e0608c0fcced46
procodong <juhovilhelm@gmail.com>
2024-08-30 18:32:54 +0300

    initial commit

---
+++ BitTorrent/Torrent/Peer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitTorrent.Torrent;
+
+public class Peer
+{
+    private readonly TcpClient _client;
+    private bool interested = false;
+    private bool choked = false;
+
+    public async static Task<Peer> Connect(string address, int port)
+    {
+        var client = new TcpClient();
+        await client.ConnectAsync(address, port);
+        var peer = new Peer(client);
+        return peer;
+    }
+
+    private Peer(TcpClient client)
+    {
+        _client = client;
+    }
+}
